4757636d935c8b9260b23afb12e321c3a14055bb
MS_Note <orc1226@naver.com>
2015-12-04 21:58:47 +0900

    bottombar 장착

--- app/src/main/java/tourplatform/drunkdiary/MainActivity.java
+++ app/src/main/java/tourplatform/drunkdiary/MainActivity.java
@@ -1,7 +1,11 @@
 package tourplatform.drunkdiary;
 
+import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CalendarView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //noinspection ResourceType
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.actionbar);
+
+
+
+        CalendarView calendarView = new CalendarView(this);
+
+    }
+
+    public void mOnClick(View view){
     }
 }
